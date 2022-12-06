7e80bfac72e72b85a430b7bee4c6c8520d10f502
jerehellstrom <72606337+jerehellstrom@users.noreply.github.com>
2022-12-06 12:44:00 +0200

    Hakupalkki joka filtteroi

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,15 +1,91 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import { EventService } from 'src/app/services/event.service';
+import { environment } from 'src/environments/environment';
+/*import { ApiResult } from 'src/app/services/event.service'; */
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
+
 export class SearchPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+    searchTerm!: string;
+    art:any = [];
+    sport:any = [];
+    music:any = [];
+    events:any = [];
+    currentPage = 1;
+    imageBaseUrl = environment.images;
+    apiKey = environment.apiKey;
+   
+  
+    constructor(private eventService: EventService, private loadingCtrl: LoadingController) {}
+  
+    ngOnInit() {
+     this.loadEvents();
+    }
+  
+    async loadEvents(){
+      const loading = await this.loadingCtrl.create({
+        message: 'Loading...',
+        spinner: 'bubbles'
+      });
+      await loading.present();
+  
+       this.eventService.getTopRatedEvents(this.currentPage).subscribe((res) =>  {
+        loading.dismiss();
+     
+        const obj = res._embedded;
+  /*       for (let key of Object.keys(this.events)) {
+          console.log(obj[0]);
+        } */
+        for (let key in obj) {
+          this.events.push(...res._embedded[key]);
+          console.log(obj[key]);
+          
+        }
+      });
+  
+      this.eventService.getMusicEvents(this.currentPage).subscribe((res) =>  {
+        loading.dismiss();
+        const obj = res._embedded;
+  
+        for (let key in obj) {
+        this.music.push(...res._embedded[key]);
+        console.log(res);
+        }
+        
+      });
+  
+      this.eventService.getSportEvents(this.currentPage).subscribe((res) =>  {
+        loading.dismiss();
+        const obj = res._embedded;
+  
+        for (let key in obj) {
+        this.sport.push(...res._embedded[key]);
+        console.log(res);
+        }
+        
+      });
+  
+      this.eventService.getArtEvents(this.currentPage).subscribe((res) =>  {
+        loading.dismiss();
+        const obj = res._embedded;
+  
+        for (let key in obj) {
+        this.art.push(...res._embedded[key]);
+        console.log(res);
+        }
+        
+      });
+      
+    }
+    loadMore(event: InfiniteScrollCustomEvent) {
+      this.currentPage++;
+      this.loadEvents();
+    }
   }
-
-}
+  
